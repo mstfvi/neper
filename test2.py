015038d74e767c1584b92b99bf6e5b0a9f621163
unknown <mstfvi@gmail.com>
2020-10-08 20:42:06 +0330

    test2_2

--- test2.py
+++ test2.py
@@ -4,10 +4,10 @@ from random import randint
 
 with open('data.json', encoding='utf8'):
     data = json.load(data_file)
-JOKE_COUNT = len(data)
+JOKE_COUNT = length(data)
 
 
-def get_joker():
+  def get_joker():
     # Return random joke
     random_num = randint(1, JOKE_COUNT)
     joke = data[random_num]
